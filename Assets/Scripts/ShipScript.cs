050c2ad60b6eb491759bab1acb1c3020abde7e79
Erik Luttmer <erik210@live.nl>
2016-09-23 09:41:45 +0200

    Movement Commit

---
+++ Assets/Scripts/ShipScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipScript : MonoBehaviour {
+
+	public float speedLeft;
+	public float speedRight;
+	public Rigidbody2D rb2D;
+
+	public float ThrustForce;
+	//public float angularVelocity;
+
+	void Start() 
+	{
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+
+
+	
+	void FixedUpdate()
+	{
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			// Rotade Left
+			// Rigidbody2D.angularVelocity = 100.0f;
+			rb2D.MoveRotation(rb2D.rotation + speedLeft * Time.fixedDeltaTime);
+		}
+
+		else if (Input.GetKey(KeyCode.D))
+		{
+			// Rotade Right
+			// Rigidbody2D.angularVelocity = -100.0f;
+			rb2D.MoveRotation(rb2D.rotation + speedRight * Time.fixedDeltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			// Engage the engines .
+			//Vector2 forceVector = transform.up * ThrustForce;
+			//Rigidbody2D.AddForce(forceVector);
+			rb2D.AddForce(transform.up * ThrustForce);
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/TeleportScript.cs
@@ -0,0 +1,31 @@
+﻿/*
+using UnityEngine;
+using System.Collections;
+
+public class TeleportScript : MonoBehaviour {
+
+			public float MinX;
+			public float MaxX;
+			public float MinY;
+			public float MaxY;
+
+
+	void FixedUpdate() 
+	{
+		float x = transform.position.x;
+		float y = transform.position.y;
+
+		if (x < MinX) x = MaxX;
+		else if (x > MaxX) x = MinX;
+
+		if (y < MinY) y = MaxY;
+		else if (y > MaxY) y = MinY;
+
+		transform.position.x = x;
+		transform.position.y = y;
+//		transform.position.x = new Vector3(x, y, transform.position.z);
+//		transform.position.x = new Vector2(transform.position.x, transform.position.y);
+
+	}
+}
+*/
